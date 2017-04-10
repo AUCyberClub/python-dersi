13f928661442f260610ab8a6b63ebf5307cc9e5c
Mucahit Karadag <mucahitkaradag@users.noreply.github.com>
2017-04-10 23:33:35 +0300

    Create fizzbuzz.py

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,10 @@
+a = int(input(""))
+for i in range(1,a+1):
+    if (i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz")
+    elif(i%3==0):
+        print("Fizz")
+    elif(i%5==0):
+        print("Buzz")
+    else:
+        print(i)
